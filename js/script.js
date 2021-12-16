2ba439f7a7dcc77746cd3a534b6e7d9a2b00ef96
Faizan <macbookair@FA.local>
2021-12-16 23:34:04 +0530

    build by temlin

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var acc = document.getElementsByClassName("accordion");
+
+
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+var slideIndex = 0;
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+showSlides();
+window.addEventListener('scroll',()=>{
+  if(document.querySelector('#aboutus').getBoundingClientRect().top < 87){
+    document.querySelector('.main-nav').classList.add('scroll')
+  } else {
+    document.querySelector('.main-nav').classList.remove('scroll')
+  }
+})
+
