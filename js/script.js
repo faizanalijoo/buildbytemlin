54b95a152be8b3da62f2f56106b4822d17b0ff73
Faizan <macbookair@FA.local>
2021-12-27 17:51:14 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -41,3 +41,28 @@ window.addEventListener('scroll',()=>{
   }
 })
 
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  loop: true,
+  keyboard: {
+    enabled: true,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
